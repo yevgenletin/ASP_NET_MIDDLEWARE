879c26700bdabaf67373de560899524f90a2e7c1
yevge <yevge@YEVGEN>
2024-02-25 18:23:53 +0100

    Agregar archivos de proyecto.

---
+++ Exceptions/ApiException.cs
@@ -0,0 +1,8 @@
+﻿namespace ASP_NET_MIDDLEWARE.Exceptions
+{
+    public class ApiException: Exception
+    {
+        public ApiException() :base(){ }
+        public ApiException(string messagge) :base(messagge){ }
+    }
+}
---
+++ Middleware/ErrorHandlerMiddleware.cs
@@ -0,0 +1,29 @@
+﻿namespace ASP_NET_MIDDLEWARE.Middleware
+{
+    public class ErrorHandlerMiddleware
+    {
+
+        public readonly RequestDelegate _next;
+        public ErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            { 
+                await _next(context);
+                    
+            }
+            catch (Exception ex)
+            {
+                switch (ex)
+                {
+                    
+                }
+                throw;
+            }
+        }
+    }
+}
